87144b80854cdafa02c631e621b5d0e650e99eaf
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-12-28 11:14:31 +0900

    게임 컨텍스트 추가 (임시로 구조체로 선언)

--- Source/Src/Main.cpp
+++ Source/Src/Main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 
+// TODO: 임시로 클래스가 아닌 구조체로 선언 (나중에 class로 변경)
+struct GameContext
+{
+	int32_t board[10][10] = { 0, };
+};
+
 int main(void)
 {
+	GameContext context;
+
 	for (int y = 0; y < 10; ++y)
 	{
 		for (int x = 0; x < 10; ++x)
 		{
 			if (x == 0 || x == 9 || y == 0 || y == 9)
+			{
+				context.board[x][y] = 1;
+			}
+		}
+	}
+
+	for (int y = 0; y < 10; ++y)
+	{
+		for (int x = 0; x < 10; ++x)
+		{
+			if (context.board[x][y] == 1)
 			{
 				std::cout << "#";
 			}
@@ -15,6 +34,7 @@ int main(void)
 				std::cout << " ";
 			}
 		}
+
 		std::cout << "\n";
 	}
 
