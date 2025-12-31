61ba8936d4e6fb4450ae983633af8d44d4e5a022
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-12-31 18:01:14 +0900

    게임 내에서 액션을 수행하는 액터 인터페이스 및 생성/파과를 수행하는 매니저 추가

---
+++ Source/Inc/IActor.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameMacro.h"
+
+class IActor
+{
+public:
+	IActor() = default;
+	virtual ~IActor() = default;
+
+	virtual void Tick(float deltaSeconds) = 0;
+	virtual void Render() = 0;
+	virtual void Release() = 0;
+
+	bool IsInitialized() { return _isInitialized; }
+
+protected:
+	bool _isInitialized = false;
+};
