e64ff1c4cc58447f344d25d5a44fcc574a0a4968
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2026-01-25 13:16:49 +0900

    이전에 GameContext에 있던 열거형을 별도의 헤더로 분리

---
+++ Game/Source/Inc/Enum.h
@@ -0,0 +1,26 @@
+#pragma once
+
+enum class ETile
+{
+	EMPTY = 0x00,
+	WALL  = 0x01,
+	BODY  = 0x02,
+	HEAD  = 0x03,
+	FOOD  = 0x04,
+};
+
+enum class EMoveDirection
+{
+	NONE  = 0x00,
+	LEFT  = 0x01,
+	RIGHT = 0x02,
+	UP    = 0x03,
+	DOWN  = 0x04,
+};
+
+enum class EMoveResult
+{
+	BLOCKED = 0x00,
+	MOVED   = 0x01,
+	CONSUME = 0x02, // ∏‘¿Ã∏¶ ∏‘¿Ω
+};
